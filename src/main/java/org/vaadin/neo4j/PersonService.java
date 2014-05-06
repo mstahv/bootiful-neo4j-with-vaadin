e45be7d602c338573b053ea2956c7c22940ed9af
Matti Tahvonen <matti@vaadin.com>
2014-05-06 22:00:30 +0300

    Some clean up based on suggestions by Josh Long

--- src/main/java/org/vaadin/neo4j/PersonService.java
+++ src/main/java/org/vaadin/neo4j/PersonService.java
@@ -4,25 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
 import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.vaadin.domain.Person;
 
-@Repository
-@Scope(value = "singleton")
+@Service
+@Transactional
 public class PersonService {
 
     @Autowired
     PersonRepository personRepository;
 
-    @Transactional
     public List<Person> allAsList() {
         return personRepository.findAll(new Sort("name")).as(ArrayList.class);
     }
 
-    @Transactional
     public void saveWithConnections(Person entity) {
         Set<Person> teammates = entity.getTeammates();
         Person saved = personRepository.save(entity);
